f5e9da66912d7288cf41c3a09875d948efefef99
jz-dot-meng <78452670+jz-dot-meng@users.noreply.github.com>
2021-04-21 22:07:45 +1000

    don't have axios...

--- js/lastfmApi_fetch.js
+++ js/lastfmApi_fetch.js
@@ -1,31 +1,17 @@
 const recentlyplayed = document.getElementById("recentlyplayed");
-// spotify scrobbles to last.fm, which has an api call 
 
+// spotify scrobbles to last.fm, which has an api call 
   // base api: 'http://ws.audioscrobbler.com/2.0/'
   // xml: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b";
   // json url for recently played: '/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=c81544cf4996253ffb848a8b771c506b&format=json'
 
 
-
-function callScrobble(path) {
-    return new Promise(function (resolve, reject) {
-        axios.get(path).then(
-            (response) => {
-                var result = response.data;
-                console.log('Processing Request');
-                resolve(result); // do i need .responseXML?
-            },
-                (error) => {
-                reject(error);
-            }
-        );
-    });
-}
-
-async function main(){
-  const result = await callScrobble("https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b");
+async function callScrobble() {
+  // await code here
+  let result = await makeRequest("GET", "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b");
+  // code below here will only execute when await makeRequest() finishes loading
   recentlyplayed.innerHTML = "<table>";
-  const track = result.getElementsByTagName("track");
+  let track = result.getElementsByTagName("track");
   for(i=0;i<track.length;i++){
     recentlyplayed.innerHTML += "<tr><td><img src='";
     recentlyplayed.innerHTML += track[i].childNodes[15].innerHTML;
@@ -38,7 +24,33 @@ async function main(){
   recentlyplayed.innerHTML += "</table>";
   console.log(recentlyplayed.innerHTML);
 }
-main();
 
+document.addEventListener("DOMContentLoaded", function(){
+  
+});
 
+function makeRequest(method, url) {
+  return new Promise(function (resolve, reject){
+    let xmlhttp = new XMLHttpRequest();
+    xmlhttp.open(method, url, true);
+    xmlhttp.onload = function(){
+      if(this.status >= 200 && this.status < 300){
+        resolve(xmlhttp.responseXML);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xmlhttp.statusText
+        });
+      }
+    };
+    xmlhttp.onerror = function(){
+      reject({
+          status: this.status,
+          statusText: xmlhttp.statusText
+      });
+    };
+    xmlhttp.send();
+  });
+}
 
+callScrobble();
