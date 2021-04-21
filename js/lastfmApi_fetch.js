6487b03498207b4a272a38680044f4a624546d33
jz-dot-meng <78452670+jz-dot-meng@users.noreply.github.com>
2021-04-21 18:16:13 +1000

    change to xmlhttprequest

--- js/lastfmApi_fetch.js
+++ js/lastfmApi_fetch.js
@@ -2,21 +2,24 @@ const recentlyplayed = document.getElementById("recentlyplayed");
 
 // spotify scrobbles to last.fm, which has an api call 
 
-function getScrobbleInfo(){
-  const lastfmApi = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b";
   // base api: 'http://ws.audioscrobbler.com/2.0/'
+  // xml: "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b";
   // json url for recently played: '/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=c81544cf4996253ffb848a8b771c506b&format=json'
-  
-  parser = new DOMParser();
-  
-  fetch(lastfmApi)
-    .then(function(response){
-      const tracks = parser.parseFromString(response, "application/xml");
-      recentlyplayed.innerHTML += tracks;
-    })
-    .catch(function(error){
-      console.log(error);
-    });
+
+
+
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+      myFunction(xmlhttp);
+    }
+  };
+  xmlhttp.open("GET", "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b", true);
+  xmlhttp.send();
+}
+function myFunction(xml) {
+  var xmlDoc = xml.responseXML;
+  recentlyplayed.innerHTML = xmlDoc;
 }
 
-getScrobbleInfo();
