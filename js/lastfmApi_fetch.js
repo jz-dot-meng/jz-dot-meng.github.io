39113d1946a24f78c5897b54dd691bad6ad17402
jz-dot-meng <78452670+jz-dot-meng@users.noreply.github.com>
2021-04-21 18:02:35 +1000

    change from http to https

--- js/lastfmApi_fetch.js
+++ js/lastfmApi_fetch.js
@@ -3,7 +3,7 @@ const recentlyplayed = document.getElementById("recentlyplayed");
 // spotify scrobbles to last.fm, which has an api call 
 
 function getScrobbleInfo(){
-  const lastfmApi = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b";
+  const lastfmApi = "https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b";
   // base api: 'http://ws.audioscrobbler.com/2.0/'
   // json url for recently played: '/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=c81544cf4996253ffb848a8b771c506b&format=json'
   
