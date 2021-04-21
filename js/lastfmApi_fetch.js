c013719e76d092fe01f542a1fd12287871b15861
jz-dot-meng <78452670+jz-dot-meng@users.noreply.github.com>
2021-04-21 18:01:06 +1000

    change to xml response

--- js/lastfmApi_fetch.js
+++ js/lastfmApi_fetch.js
@@ -3,16 +3,15 @@ const recentlyplayed = document.getElementById("recentlyplayed");
 // spotify scrobbles to last.fm, which has an api call 
 
 function getScrobbleInfo(){
-  const lastfmApi = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b&format=json";
+  const lastfmApi = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=31945ed15b54754af0b0a1c93a4a269b";
   // base api: 'http://ws.audioscrobbler.com/2.0/'
   // json url for recently played: '/2.0/?method=user.getrecenttracks&user=mengbeats&api_key=c81544cf4996253ffb848a8b771c506b&format=json'
+  
+  parser = new DOMParser();
+  
   fetch(lastfmApi)
     .then(function(response){
-      return response.json();
-    })
-    .then(function(json){
-      console.log(json);
-      var tracks = json.message;
+      const tracks = parser.parseFromString(response, "application/xml");
       recentlyplayed.innerHTML += tracks;
     })
     .catch(function(error){
