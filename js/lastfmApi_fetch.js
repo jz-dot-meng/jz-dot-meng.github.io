aca1c07a2152f6e66eada19db8ece5798c2c84a7
jz-dot-meng <78452670+jz-dot-meng@users.noreply.github.com>
2021-09-01 11:56:25 +1000

    emoji!

--- js/lastfmApi_fetch.js
+++ js/lastfmApi_fetch.js
@@ -18,7 +18,7 @@ async function callScrobble() {
   isCurrent = Date.parse(isCurrent);
   let howCurrent = date - isCurrent;
   if(howCurrent<60000000){
-    lastlistened.innerHTML+= "<p>Currently listening to: </p>";
+    lastlistened.innerHTML+= "<p>&#128308;LIVE Currently listening to: </p>";
     recentlyplayed.innerHTML += "<table><tbody><tr><td><img src='"+track[0].childNodes[15].innerHTML+"'></td><td>"+track[0].childNodes[3].innerHTML+"</td><td>"+track[0].childNodes[1].innerHTML+"</td></tr></tbody></table>";
     let otherlistened = document.createElement("div");
     otherlistened.setAttribute("id","otherlistened");
