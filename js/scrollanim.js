68ae9343c5f0cc0e60a03afcfae9eeac6abfa293
jz-dot-meng <78452670+jz-dot-meng@users.noreply.github.com>
2021-09-08 20:44:34 +1000

    does this make it continuous?

--- js/scrollanim.js
+++ js/scrollanim.js
@@ -1,3 +1,6 @@
+let contScroll=0;
+
 window.addEventListener('scroll', () => {
-  document.documentElement.style.setProperty('--scroll',window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  contScroll++;
+  document.documentElement.style.setProperty('--scroll',(contScroll%(document.body.offsetHeight - window.innerHeight)) / (document.body.offsetHeight - window.innerHeight));
 }, false);
