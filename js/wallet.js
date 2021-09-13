628fb8694075e0c1162b415205552e5dbb98acdc
Jeffrey Zhang <78452670+jz-dot-meng@users.noreply.github.com>
2021-09-14 08:51:37 +1000

    add stellar api, and doughnut graph

--- js/wallet.js
+++ js/wallet.js
@@ -1,10 +1,8 @@
-const option = ["choose...", "Polkadot"];
-
+const option = ["choose...", "Polkadot", "Stellar"];
 
 let currency = 'aud';
 function changeAUD() {
     currency = 'aud';
-    // eventually change to css visual cues
     alert("currency: " + currency);
 }
 function changeUSD() {
@@ -12,9 +10,23 @@ function changeUSD() {
     alert("currency: " + currency);
 }
 
+let piechartData = [];
+const ctx = document.getElementById("piechart").getContext("2d");
+const dataChart = new Chart(ctx, {
+    type: "doughnut",
+    data: {
+        labels: [],
+        datasets: [{
+            data: [],
+            backgroundColor: [],
+        }]
+
+    },
+})
+
 let rowcount = 0;
 
-const table = document.getElementById("assetTable")
+const table = document.getElementById("assetTable");
 
 function addAddress() {
     let temprow = rowcount;
@@ -48,7 +60,99 @@ function generateForm(item, rowcount) {
             break;
         case "1":
             // fill rest of row out
-            dotcreator(rowcount)
+            dotcreator(rowcount);
+            break;
+        case "2":
+            xlmcreator(rowcount);
+            break;
+    }
+}
+
+function xlmcreator(rowcount) {
+    let row = document.getElementById(rowcount);
+    let testelement = document.getElementById("address" + rowcount);
+    // if item doesnt already exist:
+    if (testelement == null) {
+        // really should refactor this, but how to pass elements to eventlistener?
+        // add address cell
+        let addressCell = document.createElement("td");
+        row.appendChild(addressCell);
+        // add address input
+        let address = document.createElement("input");
+        address.setAttribute("id", "address" + rowcount);
+        address.setAttribute("type", "text");
+        addressCell.appendChild(address);
+        // add amount cell
+        let amountCell = document.createElement("td");
+        row.appendChild(amountCell);
+        // add amount div
+        let amount = document.createElement("div");
+        amount.setAttribute("id", "amount" + rowcount);
+        amountCell.appendChild(amount);
+        // add loader
+        let loader1 = document.createElement('div');
+        loader1.setAttribute("class", 'loader');
+        loader1.style.display = "none";
+        amountCell.appendChild(loader1);
+        // add value cell
+        let valueCell = document.createElement("td");
+        row.appendChild(valueCell);
+        // add value div
+        let value = document.createElement("div");
+        value.setAttribute("id", "value" + rowcount);
+        valueCell.appendChild(value);
+        // add loader2
+        let loader2 = document.createElement('div');
+        loader2.setAttribute("class", 'loader');
+        loader2.style.display = "none";
+        valueCell.appendChild(loader2);
+
+        address.addEventListener('focusout', async function () {
+            loader1.style.display = "block";
+            loader2.style.display = "block";
+            let response = await fetch("https://rocky-beyond-27768.herokuapp.com/teststellar/address/" + address.value);
+            if (response.ok) {
+                let json = await response.json();
+                if (!Object.keys(json).length) {
+                    address.value = "";
+                } else {
+                    let tokenArr = json["Balances"];
+                    loader1.style.display = 'none';
+                    for (let i = 0; i < tokenArr.length; i++) {
+                        let tokenDiv = document.createElement("div");
+                        tokenDiv.setAttribute("id", "xlmToken" + i);
+                        amount.appendChild(tokenDiv);
+                        tokenDiv.innerText = tokenArr[i]["Balance"] + " " + tokenArr[i]["Asset_code"];
+                    }
+
+                    let gecko = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=stellar&vs_currencies=" + currency);
+                    if (gecko.ok) {
+                        let jsonB = await gecko.json();
+                        loader2.style.display = "none";
+                        for (let i = 0; i < tokenArr.length; i++) {
+                            let tokenValDiv = document.createElement('div');
+                            tokenValDiv.setAttribute("id", 'xlmValToken' + i);
+                            value.appendChild(tokenValDiv);
+                            // if not xlm, retrieve xlm conversion
+                            if (tokenArr[i]["Asset_code"] == "XLM") {
+                                tokenValDiv.innerText = currency.toUpperCase() + "$" + (Number(jsonB["stellar"][currency]) * Number(tokenArr[i]["Balance"]));
+                                piechartData.push({ "coin": tokenArr[i]["Asset_code"], "value": (Number(jsonB["stellar"][currency]) * Number(tokenArr[i]["Balance"])) });
+                                redrawPie();
+                            } else if (tokenArr[i]["Asset_code"] != "XLM" && tokenArr[i]["Latest_bid_to_xlm"] != "not_available") {
+                                tokenValDiv.innerText = currency.toUpperCase() + "$" + (Number(jsonB["stellar"][currency]) * Number(tokenArr[i]["Balance"]) * Number(tokenArr[i]["Latest_bid_to_xlm"]))
+                                piechartData.push({ "coin": tokenArr[i]["Asset_code"], "value": (Number(jsonB["stellar"][currency]) * Number(tokenArr[i]["Balance"]) * Number(tokenArr[i]["Latest_bid_to_xlm"])) });
+                                redrawPie();
+                            } else {
+                                tokenValDiv.innerText = "Token conversion to XLM not found, unable to provide fiat value";
+                            }
+                        }
+                    }
+                }
+
+            }
+        })
+    } else {
+        // change event listener
     }
 }
 
@@ -98,17 +202,20 @@ function dotcreator(rowcount) {
             if (response.ok) {
                 let json = await response.json();
                 if (!Object.keys(json).length) {
+                    // ie if json is empty, indicating an error
                     address.value = "";
                 } else {
                     let numtokens = Number(json["Balance"]) / Math.pow(10, 10);
                     loader1.style.display = 'none';
-                    amount.innerText = numtokens;
+                    amount.innerText = numtokens + " DOT";
 
                     let gecko = await fetch("https://api.coingecko.com/api/v3/simple/price?ids=polkadot&vs_currencies=" + currency);
                     if (gecko.ok) {
                         let jsonB = await gecko.json();
                         loader2.style.display = "none";
                         value.innerText = currency.toUpperCase() + "$" + Number(jsonB["polkadot"][currency]) * numtokens;
+                        piechartData.push({ "coin": "DOT", "value": (Number(jsonB["polkadot"][currency]) * numtokens) });
+                        redrawPie();
                     }
                 }
             }
@@ -116,4 +223,18 @@ function dotcreator(rowcount) {
     } else {
         // change event listener
     }
+}
+
+function redrawPie() {
+    dataChart.data.labels = [];
+    dataChart.data.datasets[0]['data'] = [];
+    for (let i = 0; i < piechartData.length; i++) {
+        dataChart.data.labels.push(piechartData[i]["coin"]);
+        dataChart.data.datasets[0]['data'].push(piechartData[i]["value"]);
+        let r = Math.floor(Math.random() * 255);
+        let g = Math.floor(Math.random() * 255);
+        let b = Math.floor(Math.random() * 255);
+        dataChart.data.datasets[0]["backgroundColor"].push('rgba(' + r + ',' + g + ',' + b + ',0.85)');
+    }
+    dataChart.update();
 }
