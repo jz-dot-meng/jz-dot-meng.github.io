0f17a1702dd3861b50f885ce08d0600b317a4073
Jeff Zhang <78452670+jz-dot-meng@users.noreply.github.com>
2023-05-21 14:16:32 +1000

    test github pages ci

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,22 @@
 /**
  * @type {import('next').NextConfig}
  */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = "";
+let basePath = "/";
+
+if (isGithubActions) {
+	// trim off `<owner>/`
+	const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
+
+	assetPrefix = `/${repo}/`;
+	basePath = `/${repo}`;
+}
+
 const nextConfig = {
-	images: {
-		loader: "akamai",
-		path: "",
-	},
+	assetPrefix: assetPrefix,
+	basePath: basePath,
 };
 
 export default nextConfig;
